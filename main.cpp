ebca8bf2db9c41f84ba727fb01dfc074f0823ac7
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-04-29 08:34:54 +0000

    return 0;

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+using namespace std;
 
+
+//Recursive Function
+void StringCopy(char str[],char copy[],int i=0)
+{
+   copy[i]=str[i];
+   if(str[i]=='\0')
+   return ;
+   StringCopy(str,copy,i+1);
+}
+
+void  stringCopy(char str[],char copy[])
+{
+  int i=0;
+  for(i=0;str[i]!='\0';i++)
+   copy[i]=str[i];
+  copy[i]='\0';
+
+}
 int main() {
-  std::cout << "Hello World!\n";
+  char str[]="riyaz";
+  char copy[10];
+  stringCopy(str,copy);
+  StringCopy(str,copy);
+  cout<<copy;
+
+  cout<<copy;
+  return 0;
 }
