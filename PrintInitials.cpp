cebdc2ecbeb529eb98c19fd1b0371c95a3d64809
Md Riyaz Kazmi <mdriyazuddine17@gmail.com>
2020-04-29 14:26:09 +0000

    "md riyaz ansari";

---
+++ PrintInitials.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void printInitials(const string name)
+{
+   if(name.length()==0)
+   return ;
+   cout<<(char)toupper(name[0]);
+   for(int i=1;i<name.length()-1;i++)
+   if(name[i]==' ')
+   cout<<(char)toupper(name[i+1]);
+}
+int main() 
+{ 
+    string name = "md riyaz ansari"; 
+    printInitials(name); 
+    return 0; 
+} 
